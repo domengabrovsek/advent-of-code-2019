b641d40fda84d83ea2c8fec466989f8ab3fe567e
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-05-16 15:06:02 +0200

    ..

---
+++ test/test-day-6.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { expect } = require('chai')
+const { DFS, constructGraph, sumSteps, findStartingNode } = require('../day-6/index');
+const { readFile } = require('../helpers/helpers');
+
+describe.only('day-4 tests', () => {
+
+  describe.only('find starting node tests', () => {
+
+    const testCases = [
+      { inputFile: 'test-input.txt', expectedResult: 'COM' },
+      { inputFile: 'input.txt', expectedResult: 'COM' },
+    ]
+
+    testCases.forEach(tc => {
+      it(`starting node for ${tc.inputFile} should be ${tc.expectedResult}`, () => {
+        const input = readFile(path.join(__dirname, '../', 'day-6', tc.inputFile));
+
+        const startingNode = findStartingNode(input);
+
+        expect(startingNode).to.be.equal(tc.expectedResult);
+
+      })
+    })
+  });
+
+  const testCases = [
+    { inputFile: 'test-input.txt', expectedResult: 42 },
+    { inputFile: 'input.txt', expectedResult: 42 }
+  ]
+
+  testCases.forEach(tc => {
+    it('test', () => {
+      const input = readFile(path.join(__dirname, '../', 'day-6', tc.inputFile));
+
+      const graph = constructGraph(input);
+
+      const nodes = DFS(graph, 'COM')
+
+      const stepsSum = sumSteps(nodes);
+
+      console.log(stepsSum);
+    })
+  })
+})
