b1cd555bf1fb8a2407390db0084f37d4b1d60368
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-05-16 21:19:08 +0200

    ..

--- day-6/index.js
+++ day-6/index.js
@@ -1,74 +1,16 @@
-const DFS = (graph, startNode) => {
+const constructUniqueOrbits = (input) => {
 
-  let visited = new Set();
-  let parents = {};
-  let stack = [];
+  const uniques = new Set();
 
-  // start node
-  parents[startNode] = null;
-  visited.add(startNode);
-  stack.push(startNode);
+  input.forEach(row => {
+    const [left, right] = row.split(')');
 
-  // while we have elements on stack we iterate
-  while (stack.length > 0) {
+    uniques.add(left);
+    uniques.add(right);
 
-    // stack.peek()
-    let currentNode = stack[stack.length - 1];
-    let found = false;
-
-    // check adjacent nodes (children)
-    const nodes = graph.get(currentNode);
-    const adjacentNodes = nodes ? Array.from(nodes) : [];
-
-    for (let adjacent of adjacentNodes) {
-
-      // if not visited yet then add it to stack
-      if (!visited.has(adjacent)) {
-        visited.add(adjacent);
-        parents[adjacent] = currentNode;
-        stack.push(adjacent);
-
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      stack.pop();
-    }
-  }
-
-  return parents;
-}
-
-const constructGraph = (input) => {
-  const map = new Map();
-  input.forEach(entry => {
-    const [source, destination] = entry.split(')');
-
-    if (!map.has(source)) {
-      map.set(source, new Set(destination));
-    } else {
-      map.set(source, map.get(source).add(destination));
-    }
   })
 
-  return map;
-}
-
-const findStartingNode = (input) => {
-
-  const rightList = input.map(row => row.split(')')[1]);
-
-  console.log(rightList)
-
-  for(let row of input) {
-    const [left] = row.split(')')
-
-    if(!rightList.includes(left)) {
-      return left;
-    }
-  }
+  return uniques;
 }
 
 const sumSteps = (nodes) => {
@@ -90,7 +32,5 @@ const sumSteps = (nodes) => {
 
 module.exports = {
   sumSteps,
-  constructGraph,
-  DFS,
-  findStartingNode
+  constructUniqueOrbits
 }
